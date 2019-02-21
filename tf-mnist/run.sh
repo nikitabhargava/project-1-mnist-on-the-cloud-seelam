e8ece8c58239f55797ed9696a04c20b5fdf55a6d
SEETHARAMI R. SEELAM <sseelam@us.ibm.com>
2019-02-21 16:13:32 -0500

    updates

---
+++ tf-mnist/run.sh
@@ -0,0 +1,4 @@
+#bx ml to run the training
+#bx ml to list trining runs
+bx ml list training-runs
+
---
+++ tf-mnist/set-user.sh
@@ -0,0 +1,4 @@
+export ML_ENV=https://us-south.ml.cloud.ibm.com
+export ML_USERNAME=131c1ac3-cc8d-4daa-8c58-9a321bb5e067
+export ML_PASSWORD=9ac16625-919d-4071-97c2-4e0ff0321cb4
+export ML_INSTANCE=95fb5ab0-9a6b-4ece-a8d3-345fd1bf9e4f
